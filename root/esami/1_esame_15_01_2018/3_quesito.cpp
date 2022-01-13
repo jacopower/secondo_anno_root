c2c93dba6905a43b7aeb0656ab08effa8d951faf
Gianchix <giancarlo.oancia@studio.unibo.it>
2022-01-13 12:51:55 +0100

    esami

---
+++ root/esami/1_esame_15_01_2018/3_quesito.cpp
@@ -0,0 +1,4 @@
+// Scrivi una Macro in cui
+// 1) Si definisce un istogramma monodimensionale di 100 bin in un range da 0 a 10.
+// 2) Si riempe l’istogramma con 10^7 occorrenze di una variabile casuale x distribuita secondo la p.d.f. 
+// f(x) = sqrt(x) + x^2 nel range [0,10] , utilizzando il metodo FillRandom(const char* f,Int_t N) della classe di istogrammi.
