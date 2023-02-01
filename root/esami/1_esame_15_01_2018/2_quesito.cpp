8670b522f1691da6b5457fa345f57edf73b44e32
jacopower <119303726+jacopower@users.noreply.github.com>
2023-02-01 03:12:23 +0100

    Update 2_quesito.cpp

--- root/esami/1_esame_15_01_2018/2_quesito.cpp
+++ root/esami/1_esame_15_01_2018/2_quesito.cpp
@@ -16,6 +16,7 @@
 #include "TCanvas.h"
 #include "TMath.h"
 
+
 void setStyle()
 {
   gROOT->SetStyle("Plain");
@@ -24,57 +25,59 @@ void setStyle()
   gStyle->SetOptFit(1111);
 }
 
-// Gaus + A (par[3])
-Double_t myFunction(Double_t *x, Double_t *par)
+
+Double_t myFunction(Double_t *x, Double_t *par)     // gauss distrib function + polinomio ( = par[3])
 {
   Double_t xx = x[0];
   Double_t val = par[0] * TMath::Exp(-(xx - par[1]) * (xx - par[1]) / 2. / par[2] / par[2]) + par[3];
   return val;
-}
+}     // par[0] =ampiezza ; par[1] = media ; par[2] = dev st
+      // why par[0] and not just []
+
 
 void myMacro()
 {
   constexpr Int_t nGen1 = 1E6;
   constexpr Int_t nGen2 = 1E4;
 
-  TH1F *h1 = new TH1F("h1", "histo1", 500, 0., 5.);
-  TH1F *h2 = new TH1F("h2", "histo2", 500, 0., 5.);
+  TH1F *h1 = new TH1F("h1", "histo1", 500, 0., 5.);     // creating histo 1
+  TH1F *h2 = new TH1F("h2", "histo2", 500, 0., 5.);     // creating histo 2
 
   Double_t x1, x2 = 0;
-  for (Int_t i = 0; i < nGen1; ++i)
+  for (Int_t i = 0; i < nGen1; ++i)     // generating x and filling h1 1E6 times
   {
-    x1 = gRandom->Gaus(2.5, 0.25);
-    h1->Fill(x1);
+    x1 = gRandom->Gaus(2.5, 0.25);      // generating x from a gauss distribution of mean=2.5 and stdev=0.25
+    h1->Fill(x1);                       // filling h1 with x
   }
 
-  for (Int_t i = 0; i < nGen2; ++i)
+  for (Int_t i = 0; i < nGen2; ++i)     // filling h2 ...
   {
     x2 = gRandom->Uniform(0., 5.);
     h2->Fill(x2);
   }
 
-  TH1F *hSum = new TH1F(*h1);
+  TH1F *hSum = new TH1F(*h1);           // creating new histo
   hSum->SetTitle("Somma Istogrammi");
   hSum->SetName("hSum");
-  hSum->Add(h1, h2, 1, 1);
+  hSum->Add(h1, h2, 1, 1);              // hSum = h1 + h2
 
-  TF1 *f = new TF1("f", myFunction, 0., 5., 4);
-  f->SetParameters(1, 1, 1, 1);
+  TF1 *f = new TF1("f", myFunction, 0., 5., 4);     // creating functional form with myFunction in [0,5]
+  f->SetParameters(1, 1, 1, 1);                     // why ???
   f->SetLineColor(kRed);
   f->SetLineWidth(1);
 
   TCanvas *canvas = new TCanvas("canvas");
-  hSum->Fit("f");
-  hSum->Draw();
+  hSum->Fit("f");                     // fittin hSum with the user-defined funtion f
+  hSum->Draw();                       // draw hSum on canvas
 
-  TCanvas *c = new TCanvas("c");
+  TCanvas *c = new TCanvas("c");      // wt is that ???
   c->Divide(1, 2);
   c->cd(1);
   h1->Draw();
   c->cd(2);
   h2->Draw();
 
-  TF1 *fitFunc = hSum->GetFunction("f");
+  TF1 *fitFunc = hSum->GetFunction("f");            // idk why i need this ???
   Double_t ampiezza = fitFunc->GetParameter(0);
   Double_t media = fitFunc->GetParameter(1);
   Double_t stdDev = fitFunc->GetParameter(2);
